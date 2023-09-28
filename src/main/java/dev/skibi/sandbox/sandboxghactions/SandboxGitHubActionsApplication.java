f5781c224c33aee971bbbe1715dc84b9e6e0aa67
skibi <kijasmichal@gmail.com>
2023-09-28 20:31:55 +0200

    Added simple Spring application with `Hello world` controller

---
+++ src/main/java/dev/skibi/sandbox/sandboxghactions/SandboxGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package dev.skibi.sandbox.sandboxghactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxGitHubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxGitHubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/skibi/sandbox/sandboxghactions/SandboxGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skibi.sandbox.sandboxghactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxGitHubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
